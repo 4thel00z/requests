8ec5c51ac4b50ad02e793a57b646e0bcabeaa6e0
4thel00z <4thel00z@gmail.com>
2020-08-13 14:40:15 +0200

    [master] Add debug versions of the functions to print out the requests as curl commands

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/4thel00z/requests
 
 go 1.14
+
+require (
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	moul.io/http2curl v1.0.0
+)
--- requests.go
+++ requests.go
@@ -1,7 +1,9 @@
 package requests
 
 import (
+	"fmt"
 	"io"
+	"moul.io/http2curl"
 	"net/http"
 )
 
@@ -42,6 +44,43 @@ func Get(url string, params, headers *map[string]string, proxy *http.Transport, 
 
 }
 
+func GetDebug(url string, params, headers *map[string]string, proxy *http.Transport, cookies *[]http.Cookie) (resp *http.Response, err error) {
+	var client = http.DefaultClient
+
+	if proxy != nil {
+		client = &http.Client{Transport: proxy}
+	}
+	request, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if params != nil && len(*params) != 0 {
+		q := request.URL.Query()
+		for k, v := range *params {
+			q.Add(k, v)
+		}
+		request.URL.RawQuery = q.Encode()
+	}
+	if headers != nil {
+		for k, v := range *headers {
+			request.Header.Add(k, v)
+		}
+	}
+	if cookies != nil {
+		for _, cookie := range *cookies {
+			request.AddCookie(&cookie)
+		}
+	}
+
+	command, err := http2curl.GetCurlCommand(request)
+	fmt.Println(command)
+
+	return client.Do(request)
+
+}
+
 func Post(url string, body io.Reader, headers *map[string]string, proxy *http.Transport, cookies *[]http.Cookie) (resp *http.Response, err error) {
 	var client = http.DefaultClient
 	if proxy != nil {
@@ -62,6 +101,33 @@ func Post(url string, body io.Reader, headers *map[string]string, proxy *http.Tr
 			request.AddCookie(&cookie)
 		}
 	}
+
+	return client.Do(request)
+}
+
+func PostDebug(url string, body io.Reader, headers *map[string]string, proxy *http.Transport, cookies *[]http.Cookie) (resp *http.Response, err error) {
+	var client = http.DefaultClient
+	if proxy != nil {
+		client = &http.Client{Transport: proxy}
+	}
+	request, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+	if headers != nil {
+		for k, v := range *headers {
+			request.Header.Add(k, v)
+		}
+	}
+
+	if cookies != nil {
+		for _, cookie := range *cookies {
+			request.AddCookie(&cookie)
+		}
+	}
+
+	command, err := http2curl.GetCurlCommand(request)
+	fmt.Println(command)
 
 	return client.Do(request)
 }
@@ -91,5 +157,38 @@ func PostForm(target string, params, headers map[string]string, proxy *http.Tran
 			request.AddCookie(&cookie)
 		}
 	}
+
+	return client.Do(request)
+}
+
+func PostFormDebug(target string, params, headers map[string]string, proxy *http.Transport, cookies *[]http.Cookie) (resp *http.Response, err error) {
+	var client = http.DefaultClient
+	if proxy != nil {
+		client = &http.Client{Transport: proxy}
+	}
+	request, err := http.NewRequest("POST", target, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.PostForm = map[string][]string{}
+	if params != nil && len(params) != 0 {
+		for k, v := range params {
+			request.PostForm.Set(k, v)
+		}
+	}
+
+	for k, v := range headers {
+		request.Header.Add(k, v)
+	}
+
+	if cookies != nil {
+		for _, cookie := range *cookies {
+			request.AddCookie(&cookie)
+		}
+	}
+
+	command, err := http2curl.GetCurlCommand(request)
+	fmt.Println(command)
+
 	return client.Do(request)
 }
